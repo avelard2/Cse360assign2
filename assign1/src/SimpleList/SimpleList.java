6d770875a5533ea84e884edcb396ab38a5b7b717
Alexis Velarde <alexisramirez4641@gmail.com>
2020-02-27 21:48:43 -0700

    This is add and the remove with a search.

--- assign1/src/SimpleList/SimpleList.java
+++ assign1/src/SimpleList/SimpleList.java
@@ -10,4 +10,68 @@ public class SimpleList {
 		list = new int[len];
 		count = 0;
 	}
+	public void add(int n)
+	{
+		if(count == len - 1)
+		{
+			len = len + len/2;
+			int[] temp = new int[len];
+			
+			for(int i = 0; i < count; i++)
+			{
+				temp[i] = list[i];
+			}
+			list = temp;
+		}
+		
+		for(int i = count; i >= 0; i--)
+		{
+			list[i + 1] = list[i];
+		}
+		count++;
+		list[0] = n;
+	}
+	public void remove(int n)
+	{
+		int[] newList = new int[len];
+		int place = search(n);
+		for(int i = 0; i < place; i++)
+		{
+			newList[i] = list[i];
+		}
+		for(int i = place; i < count; i++)
+		{
+			newList[i] = list[i + 1];
+		}
+		list = newList;
+		count--;
+	
+	}
+	public String toString()
+	{
+		for(int i = 0; i < count; i++)
+		{
+			if(i < count - 1)
+			{
+				System.out.print(list[i] + " ");
+				
+			}
+			if(i == count - 1)
+			{
+				System.out.print(list[i] + "\n");
+			}
+		}
+		return " ";
+	}
+	public int search(int n)
+	{
+		for(int i = 0; i <= count; i++)
+		{
+			if(n == list[i])
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
 }
